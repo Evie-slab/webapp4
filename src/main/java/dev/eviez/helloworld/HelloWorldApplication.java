c2a6649339babedd1c7eefe796b89986ce070dcb
ZhengZhangXiao <113813833+ZhengZhangXiao@users.noreply.github.com>
2024-02-25 18:01:09 -0800

    push assignment3 (#1)
    * Create main.yml
    
    * Create integration-test.yml (#2)
    
    * assignment3
    
    * Update integration-test.yml (#3)
    
    * Update integration-test.yml
    
    * Update integration-test.yml
    
    * Update integration-test.yml
    
    * push 03
    
    * push3
    
    * Update integration-test.yml
    
    * Update integration-test.yml
    
    * push 003
    
    * Update integration-test.yml
    
    * push assignment03
    
    * Update integration-test.yml

---
+++ src/main/java/dev/eviez/helloworld/HelloWorldApplication.java
@@ -0,0 +1,26 @@
+package dev.eviez.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+//        System.out.println("Hello World");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
